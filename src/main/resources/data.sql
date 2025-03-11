a3552cd3257332272e79a59d4b29858ed7b66374
Jieshen Loh <111634737+LohJieshen@users.noreply.github.com>
2025-03-11 22:33:11 +0800

    Add/edit DB Data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,7 +18,8 @@ INSERT INTO course (course_id, course_name) VALUES
 INSERT INTO topic (topic_id, topic_name, course_id) VALUES
 (1, 'Data Structures and Algorithms in Java', 1),
 (2, 'Machine Learning', 2),
-(3, 'Statistics', 2);
+(3, 'Statistics', 2),
+(4, 'Object-oriented Programming in Java', 1);
 
 -- user_id format: YYXXXX. user_id is text as it's a combination of strings and numbers.
 INSERT INTO "user" (user_id, password, first_name, last_name, last_login_date, course_id, is_lecturer) VALUES
@@ -35,7 +36,10 @@ INSERT INTO "user" (user_id, password, first_name, last_name, last_login_date, c
 INSERT INTO quiz (quiz_id, quiz_name, course_id, max_attempts, due_date, is_removed) VALUES
 (1, 'DSA Revision', 1, 10, '2024-11-30', FALSE),
 (2, 'Machine Learning', 2, 3, '2024-12-30', FALSE),
-(3, 'Basic Statistics 1', 2, 2, '2024-10-30', FALSE);
+(3, 'Basic Statistics 1', 2, 2, '2024-10-30', FALSE),
+(4, 'DSA Revision 2', 1, 10, '2050-11-30', FALSE),
+(5, 'Machine Learning Revision', 2, 3, '2050-12-30', FALSE),
+(6, 'OOP Revision', 1, 10, '2050-12-30', FALSE);
 
 -- quiz_attempt: defaults for attempts and high_score can be set in PostgreSQL as well
 INSERT INTO quiz_attempt (quiz_id, user_id) VALUES
@@ -46,7 +50,17 @@ INSERT INTO quiz_attempt (quiz_id, user_id) VALUES
 (1, '230005'),
 (1, '230006'),
 (2, '230007'),
-(2, '230008');
+(2, '230008'),
+(4, '230001'),
+(4, '230003'),
+(4, '230004'),
+(4, '230005'),
+(4, '230006'),
+(6, '230001'),
+(6, '230003'),
+(6, '230004'),
+(6, '230005'),
+(6, '230006');
 
 -- question_id is SERIAL in PostgreSQL.
 INSERT INTO question (question_id, question_body, topic_id, ans_id, is_removed, choice_1, choice_2, choice_3, choice_4) VALUES
@@ -57,7 +71,12 @@ INSERT INTO question (question_id, question_body, topic_id, ans_id, is_removed, 
 (5, 'Which of the given machine learning is NOT a supervised learning technique?', 2, 3, FALSE, 'Linear Regression', 'Decision Tree', 'K-nearest neighbour', 'K-means clustering'),
 (6, 'Which of the following is NOT an ensemble machining learning model?', 2, 3, FALSE, 'Support Vector Machine', 'Random Forest', 'XGBoost', 'CatBoost'),
 (7, 'What is the median value in the following number sequence: 2, 3, 3, 4, 4, 6, 8, 10', 3, 3, FALSE, '6', '1.5', '4', '12'),
-(8, 'What is the mean value in the following number sequence: 2, 3, 3, 4, 4, 6, 8, 10', 3, 2, FALSE, '4', '5', '8', '6');
+(8, 'What is the mean value in the following number sequence: 2, 3, 3, 4, 4, 6, 8, 10', 3, 2, FALSE, '4', '5', '8', '6'),
+(9, 'What does the S in SOLID stand for?', 4, 3, FALSE, 'Special Responsibility Principle', 'Sole Responsibility Principle', 'Single Responsibility Principle', 'Single Reversible Principle'),
+(10, 'Using interfaces allows to achieve 100% abstraction', 4, 1, FALSE, 'True', 'False', NULL, NULL),
+(11, 'What does the O in SOLID stand for?', 4, 2, FALSE, 'Only-Extendable Principle', 'Open-Closed Principle', 'Ordered Principle', 'Open-Extended Principle'),
+(12, 'What does the I in SOLID stand for?', 4, 4, FALSE, 'Internal Segregation', 'Inside Segregation', 'Interface Specialisation', 'Interface Segregation'),
+(13, 'What does the L in SOLID stand for?', 4, 1, FALSE, 'Liskov Substitution Principle', 'Liskov Super-sub-class Principle', 'Liskov Principle', 'Lesser Substitution Principle');
 
 -- quiz_question does not need changes as this is straightforward
 INSERT INTO quiz_question (quiz_id, question_id) VALUES
@@ -68,4 +87,15 @@ INSERT INTO quiz_question (quiz_id, question_id) VALUES
 (2, 5),
 (2, 6),
 (3, 7),
-(3, 8);
+(3, 8),
+(4, 1),
+(4, 2),
+(4, 3),
+(5, 4),
+(5, 5),
+(5, 6),
+(6, 9),
+(6, 10),
+(6, 11),
+(6, 12),
+(6, 13);
